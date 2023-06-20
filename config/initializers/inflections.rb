fc3736b4a234300db75c606ebb88712366b6aa00
tuliotm <tulioteodoromanso@gmail.com>
2023-06-20 10:17:30 -0300

    New action buttons and inflections

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -10,6 +10,11 @@
 #   inflect.uncountable %w( fish sheep )
 # end
 
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.plural 'Comentário', 'Comentários'
+  inflect.plural 'resultado', 'resultados'
+end
+
 # These inflection rules are supported but not enabled by default:
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym "RESTful"
